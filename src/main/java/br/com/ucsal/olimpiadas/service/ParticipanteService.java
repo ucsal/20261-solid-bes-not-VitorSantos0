d8c3c20d64f79a42ff8faedbd313f7587444cae6
Vitor Santos <the_vitor@hotmail.com>
2026-03-31 23:34:47 -0300

    feat criando camada de servido e finalizando os conceitos de SOLID no app

---
+++ src/main/java/br/com/ucsal/olimpiadas/service/ParticipanteService.java
@@ -0,0 +1,5 @@
+package br.com.ucsal.olimpiadas.service;
+
+public interface ParticipanteService {
+    void cadastrarParticipante();
+}
---
+++ src/main/java/br/com/ucsal/olimpiadas/service/ProvaService.java
@@ -0,0 +1,6 @@
+package br.com.ucsal.olimpiadas.service;
+
+public interface ProvaService {
+    void cadastrarProva();
+    void cadastrarQuestao();
+}
